70e7f5f63ff57945a9a27114d47e6b02071b42c8
Jack Ren <bjrjk@qq.com>
2020-05-15 08:33:49 +0800

    Behaviour Simulation Passed!

--- MIPS-Multi-Cycle-MicroSystem.srcs/sim_1/new/SIM.v
+++ MIPS-Multi-Cycle-MicroSystem.srcs/sim_1/new/SIM.v
@@ -7,16 +7,19 @@ module SIM();
     reg clk=1,rst=1;
     wire OutDev_WrEn;
     wire [`QBBus] OutDev_Data;
+    reg [`QBBus] InDev_Data=0;
     SoC insSoC(
     .clk(clk),
     .rst(rst),
     .OutDev_WrEn(OutDev_WrEn),
     .OutDev_Data(OutDev_Data),
-    .InDev_Data(0)
+    .InDev_Data(InDev_Data)
     );
 
     initial forever #1 clk=~clk;
     initial #4 rst=0;
+    initial #2500 InDev_Data=32'h80000000;
     initial #5000 $stop;
+
 
 endmodule
